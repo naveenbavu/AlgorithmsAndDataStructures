abb350f734c24e6d74a8d3cb4ed23d2e99e25d45
Naveen <naveenbavu@gmail.com>
2020-07-19 01:51:54 -0700

    SelectionSort algorithm after final execution

--- src/main/java/ds/SelectionSort.java
+++ src/main/java/ds/SelectionSort.java
@@ -2,8 +2,30 @@ package ds;
 
 public class SelectionSort {
 
- public static void main(String[] args) {
+  public static int[] selectionSort(int[] input) {
+    int temp, minindex, minvalue = 0;
+    for (int i = 0; i < input.length; i++) {
+      minindex = i;
+      minvalue = input[i];
+      for (int j = i; j < input.length; j++) {
+        if (input[j] < minvalue) {
+          minvalue = input[j];
+          minindex = j;
+        }
+      }
+      if (minvalue < input[i]) {
+        temp = input[i];
+        input[i] = input[minindex];
+        input[minindex] = temp;
+      }
+    }
+    return input;
+  }
 
+  public static void main(String[] args) {
+    int[] arr = {7, 2, 8, 1, 3, 11, 9, 19, 13};
+    int[] res = selectionSort(arr);
+    System.out.println(res);
   }
 
 }
